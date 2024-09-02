3f88546e5227a86cda20ecb5f80789a75d988e3d
Michael Langford <carnix@gmail.com>
2024-09-02 15:19:50 -0400

    docs: revised and expanded code documentation
    resolves #9

--- js/app.js
+++ js/app.js
@@ -1,5 +1,29 @@
+/**
+ * @module pishockbooks
+ * @description Entry point for pishockbooks online website application features.
+ * @author Michael Langford
+ * @version 1.0.0
+ */
+
 import { initScrollEffects } from './modules/scrollEffects.js';
 import { loadGallery } from './modules/gallery.js';
 
-await loadGallery('img/gallery/config.json');
-initScrollEffects();
+/**
+ * Loads the gallery configuration from a JSON file and initializes scroll effects.
+ * This function is called once the module has been loaded and is responsible for setting up
+ * the initial state of the application.
+ * 
+ * @async
+ * @function initializeApp
+ * @returns {Promise<void>} Resolves when both the gallery and scroll effects are initialized.
+ */
+const initializeApp = async () => {
+    // Load gallery configuration
+    await loadGallery('img/gallery/config.json');
+
+    // Initialize scroll effects
+    initScrollEffects();
+};
+
+// Execute the initialization function
+initializeApp();
